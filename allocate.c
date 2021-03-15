747b2307b9277ade31b7f188978dd3c8d75acc23
Khin Liew <khinl@student.unimelb.edu.au>
2021-03-15 16:18:26 +1100

    Added test text file

--- allocate.c
+++ allocate.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
     // command line variables
@@ -9,11 +10,13 @@ int main(int argc, char **argv) {
 
     char *fileName;
 
+    char c;
+
     while((opt = getopt(argc, argv, "f:p:c:")) != -1) {
         switch(opt) {
             case 'f':
                 fileName = optarg;
-                processesFile = fopen(fileName, 'r');
+                processesFile = fopen(fileName, "r");
                 assert(processesFile != NULL);
                 break;
             case 'p':
@@ -23,6 +26,19 @@ int main(int argc, char **argv) {
                 break;
         }
     }
+
+    fclose(processesFile);
+
+    /* DEBUGGING
+    printf("Here is the file:\n");
+    c = fgetc(processesFile);
+    while(c != EOF) {
+        printf("%c", c);
+        c = fgetc(processesFile);
+    }
+    printf("\n");
+    printf("Number of processors: %d\n", numProcessors);
+    */
 
     return 0;
 }
