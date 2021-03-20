0e302d401c60b987afa5f68cc6946341ea92543d
Khin Liew <khinl@student.unimelb.edu.au>
2021-03-20 12:50:55 +1100

    modified get opt

--- allocate.c
+++ allocate.c
@@ -34,7 +34,7 @@ int main(int argc, char **argv) {
                 coreCount = atoi(optarg);
                 break;
             case 'c': // challenge task
-                quantum = atoi(optarg);
+                quantum = 40;
                 break;
         }
     }
