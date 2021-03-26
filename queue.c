959e0db5dccffca953eb0d5192de69b2284bd54b
Khin Liew <khinl@student.unimelb.edu.au>
2021-03-26 20:16:48 +1100

    removed debugging prints from queue.c

--- queue.c
+++ queue.c
@@ -4,39 +4,17 @@
 #include "scheduling.h"
 #include "queue.h"
 
-void enQueue(struct process cpuQueue[], struct process processEntry, int *cpuRemainingTime, int *back, int *front) { // sort array by execution time
-    // struct process temp;
-    // int sortingPointer;
-
+void enQueue(struct process cpuQueue[], struct process processEntry, int *cpuRemainingTime, int *back, int *front) {
     if(*back != (numProcesses-1)) {
         if(*front == -1) {
             *front = 0;
         }
         (*back)++;
-        //printf("%d,Enqueuing %d %d %d %c...\n", clock, processEntry.timeArrived, processEntry.processId, processEntry.executionTime, processEntry.parallelisable);
         cpuQueue[*back] = processEntry;
         (*cpuRemainingTime) += processEntry.executionTime;
-        //printf("%d,Enqueued %d %d %d %c\n", clock, cpuQueue[*back].timeArrived, cpuQueue[*back].processId, cpuQueue[*back].executionTime, cpuQueue[*back].parallelisable);
 
-        // sort queue by execution time
-        // sortingPointer = *back;
-        // for(int i=0; i<(*back); i++) {
-        //     if(cpuQueue[sortingPointer].executionTime <= cpuQueue[sortingPointer-1].executionTime) {
-        //         temp = cpuQueue[sortingPointer-1];
-        //         cpuQueue[sortingPointer-1] = cpuQueue[sortingPointer];
-        //         cpuQueue[sortingPointer] = temp;
-        //         sortingPointer--;
-        //     } else {
-        //         break;
-        //     }
-        // }
-
+        // sort array by execution time
         qsort(cpuQueue, (*back)+1, sizeof(struct process), cmpTimeArrivedExecTime);
-
-        // printf("\nPrinting Unsorted Queue\n");
-        // for(int i=0; i<numProcesses; i++) {
-        //     printf("Process %d: %d %d %d %c\n", i, cpuQueue[i].timeArrived, cpuQueue[i].processId, cpuQueue[i].executionTime, cpuQueue[i].parallelisable);
-        // }
 
         // for processes with the same execution time, sort by process id
         int exec_dup = 1;
@@ -44,29 +22,17 @@ void enQueue(struct process cpuQueue[], struct process processEntry, int *cpuRem
         for(int k=0; k<size; k++) {
             if(cpuQueue[k].executionTime == cpuQueue[k+1].executionTime) {
                 exec_dup++;
-                //printf("k %d exec_dup %d\n", k, exec_dup);
                 if(k == size - 1) {
                     exec_dup--;
                 }
             }
             if(cpuQueue[k].executionTime != cpuQueue[k+1].executionTime || (k == (size - 1))) {
                 if(exec_dup > 1) {
-                    //printf("qsort %d %d\n", k-exec_dup+1, exec_dup);
                     qsort(&cpuQueue[k-exec_dup+1], exec_dup, sizeof(struct process), cmpExecTimeProcessId);
                     exec_dup = 1;
                 }
             }
         }
-        // printf("\nPrinting Queue sorted by id\n");
-        // for(int i=0; i<numProcesses; i++) {
-        //     printf("Process %d: %d %d %d %c\n", i, cpuQueue[i].timeArrived, cpuQueue[i].processId, cpuQueue[i].executionTime, cpuQueue[i].parallelisable);
-        // }
-        //
-        // print queue
-        // printf("\t\t\tPRINTING QUEUE...\n");
-        // for(int i=0; i<numProcesses; i++) {
-        //     printf("%d,\t\t\t###DEBUG###\t %d %d %d %c\n", clock, cpuQueue[i].timeArrived, cpuQueue[i].processId, cpuQueue[i].executionTime, cpuQueue[i].parallelisable);
-        // }
     }
 }
 
