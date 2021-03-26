3e6d6f64ca68dfe0ba9edbb85a7ed4bbd104c13e
Khin Liew <khinl@student.unimelb.edu.au>
2021-03-26 15:31:33 +1100

    fixed kmultiplier typo

--- scheduling.c
+++ scheduling.c
@@ -158,9 +158,9 @@ void challengeStep(struct cpu *processor, int *processesCompleted, struct proces
 // calculate how many times to split a parallelisable process for the N-Processor Scheduler (largest value of k such that x/k≥1)
 int calculateSplitCount(int time) {
     int nSub = coreCount; // number of subprocesses
-    int kMultipler = 1;
+    int kMultiplier = 1;
     if(cFlag == 1) {
-        kMultipler = 4;
+        kMultiplier = 4;
     }
     for(int i=0; i<coreCount; i++) {
         if((kMultiplier*nSub) > time) {
